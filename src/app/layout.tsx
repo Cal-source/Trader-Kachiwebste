720a9f42fb9ae5afea4c5e54a94478e439334321
Cal-source <lcaleb288@gmail.com>
2026-05-14 12:39:36 +0100

    Add RootLayout component with metadata

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Trader Kachi",
+  description: "Elite Forex Mentorship Nigeria"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-background text-white">
+        {children}
+      </body>
+    </html>
+  );
+}
